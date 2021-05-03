3b02cb6befe98db9ea4d90155fd8a38548d29923
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2021-05-03 15:40:38 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/luiz-gustavo-oliveira-cunha-a9b329176/" target="_blank" rel="noreferrer">Luiz Gustavo</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
